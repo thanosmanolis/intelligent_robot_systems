70d90c01d3f5d38d2244bb6a0453ca5cf11024b3
vagzikopis <vagzikopis@gmail.com>
2020-04-22 15:45:26 +0300

    Challenge 2: Path Visualization

--- art_autonomous_exploration/src/navigation.py
+++ art_autonomous_exploration/src/navigation.py
@@ -47,7 +47,7 @@ class Navigation:
 
         # Check if subgoal is reached via a timer callback
         rospy.Timer(rospy.Duration(0.10), self.checkTarget)
-        
+
         # Read the target function
         self.target_selector = rospy.get_param("target_selector")
         print "The selected target function is " + self.target_selector
@@ -57,17 +57,17 @@ class Navigation:
         self.path_publisher = \
             rospy.Publisher(rospy.get_param('path_pub_topic'), \
             Path, queue_size = 10)
-        
+
         # ROS Publisher for the subtargets
         self.subtargets_publisher = \
             rospy.Publisher(rospy.get_param('subgoals_pub_topic'),\
             MarkerArray, queue_size = 10)
-        
+
         # ROS Publisher for the current target
         self.current_target_publisher = \
             rospy.Publisher(rospy.get_param('curr_target_pub_topic'),\
             Marker, queue_size = 10)
-        
+
     def checkTarget(self, event):
         # Check if we have a target or if the robot just wanders
         if self.inner_target_exists == False or self.move_with_target == False or\
@@ -77,7 +77,7 @@ class Navigation:
         self.counter_to_next_sub -= 1
 
         if self.counter_to_next_sub == 0:
-          Print.art_print('\n~~~~ Time reset ~~~~',Print.RED) 
+          Print.art_print('\n~~~~ Time reset ~~~~',Print.RED)
           self.inner_target_exists = False
           self.target_exists = False
           return
@@ -96,7 +96,7 @@ class Navigation:
             ry - self.subtargets[self.next_subtarget][1])
 
         ######################### NOTE: QUESTION  ##############################
-        # What if a later subtarget or the end has been reached before the 
+        # What if a later subtarget or the end has been reached before the
         # next subtarget? Alter the code accordingly.
         # Check if distance is less than 7 px (14 cm)
         if dist < 5:
@@ -106,7 +106,7 @@ class Navigation:
           if self.next_subtarget == len(self.subtargets):
             self.target_exists = False
         ########################################################################
-        
+
         # Publish the current target
         if self.next_subtarget == len(self.subtargets):
             return
@@ -157,13 +157,13 @@ class Navigation:
         # We are good to continue the exploration
         # Make this true in order not to call it again from the speeds assignment
         self.target_exists = True
-              
+
         # Gets copies of the map and coverage
         local_ogm = self.robot_perception.getMap()
         local_ros_ogm = self.robot_perception.getRosMap()
         local_coverage = self.robot_perception.getCoverage()
         print "Got the map and Coverage"
-        self.path_planning.setMap(local_ros_ogm) 
+        self.path_planning.setMap(local_ros_ogm)
 
         # Once the target has been found, find the path to it
         # Get the global robot pose
@@ -182,9 +182,9 @@ class Navigation:
                     local_coverage,\
                     self.robot_perception.robot_pose,
                     self.robot_perception.origin,
-                    self.robot_perception.resolution, 
+                    self.robot_perception.resolution,
                     force_random)
-          
+
           self.path = self.path_planning.createPath(\
               g_robot_pose,\
               target,
@@ -196,7 +196,7 @@ class Navigation:
                 "Path planning failed. Fallback to random target selection", \
                 Print.RED)
             force_random = True
-          
+
         # Reverse the path to start from the robot
         self.path = self.path[::-1]
 
@@ -209,7 +209,7 @@ class Navigation:
         self.subtargets.append(self.path[-1])
         self.next_subtarget = 0
         print "The path produced " + str(len(self.subtargets)) + " subgoals"
-        
+
         ######################### NOTE: QUESTION  ##############################
         # The path is produced by an A* algorithm. This means that it is
         # optimal in length but 1) not smooth and 2) length optimality
@@ -230,7 +230,14 @@ class Navigation:
           # Fill the ps.pose.position values to show the path in RViz
           # You must understand what self.robot_perception.resolution
           # and self.robot_perception.origin are.
-        
+
+          # Multiply subgoal's coordinates with resolution and
+          # add robot.origin in order to translate between the (0,0) of the robot
+          # pose and (0,0) of the map
+          ps.pose.position.x = p[0] * self.robot_perception.resolution + \
+                   self.robot_perception.origin['x']
+          ps.pose.position.y = p[1] * self.robot_perception.resolution + \
+                   self.robot_perception.origin['y']
           ########################################################################
           ros_path.poses.append(ps)
         self.path_publisher.publish(ros_path)
@@ -259,9 +266,9 @@ class Navigation:
         self.inner_target_exists = True
 
     def velocitiesToNextSubtarget(self):
-        
+
         [linear, angular] = [0, 0]
-        
+
         [rx, ry] = [\
             self.robot_perception.robot_pose['x_px'] - \
                     self.robot_perception.origin['x'] / self.robot_perception.resolution,\
@@ -269,19 +276,19 @@ class Navigation:
                     self.robot_perception.origin['y'] / self.robot_perception.resolution\
                     ]
         theta = self.robot_perception.robot_pose['th']
+
         ######################### NOTE: QUESTION  ##############################
-        # The velocities of the robot regarding the next subtarget should be 
-        # computed. The known parameters are the robot pose [x,y,th] from 
-        # robot_perception and the next_subtarget [x,y]. From these, you can 
+        # The velocities of the robot regarding the next subtarget should be
+        # computed. The known parameters are the robot pose [x,y,th] from
+        # robot_perception and the next_subtarget [x,y]. From these, you can
         # compute the robot velocities for the vehicle to approach the target.
         # Hint: Trigonometry is required
 
         if self.subtargets and self.next_subtarget <= len(self.subtargets) - 1:
             st_x = self.subtargets[self.next_subtarget][0]
             st_y = self.subtargets[self.next_subtarget][1]
-            
+
+
         ######################### NOTE: QUESTION  ##############################
 
         return [linear, angular]
-
-    
